955b4c1e0b0c5c6108c3860bf9f1d17e6ba63a2c
Daniel Jack <103968885+Frankie-420@users.noreply.github.com>
2023-12-27 02:36:09 +0000

    New CML UI Addition

---
+++ UI.py
@@ -0,0 +1,23 @@
+thickness = input("Please enter a Thickness: ")
+
+print('''
+Rectangle: 
+ ___________
+|           |
+|           | - height
+|___________|
+    Width
+''')
+width = input("Please enter a width: ")
+height = input("Please enter a height: ")
+
+print('''
+Triangle:               
+          /\            
+length A /  \ Length B  
+        /____\          
+       Length C         
+''')
+length_a = input("Please enter a length A: ")
+length_b = input("Please enter a length B: ")
+length_c = input("Please enter a length C: ")
